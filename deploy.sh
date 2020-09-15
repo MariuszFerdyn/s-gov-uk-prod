259df073a7789e0150d0e09a4b5ee00c7cd8b173
Pranay Bejgum <prbejgum@microsoft.com>
2020-09-15 21:00:42 +0530

    deployment setup

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+apt-get update
+echo "Install package manager for Python packages"
+sudo apt-get install python-pip -y
+
+# install docker does not exists
+sudo apt-get update && sudo apt-get install curl git -y
+
+if [ -x "$(command -v docker)" ]; then
+    echo "docker exists"
+    # command
+else
+    echo "Install docker"
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.27.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo systemctl enable --now docker
+fi
+
+# get code and deploy
+rm -rf /tmp/k8-ova
+git clone https://github.com/filetrust/k8-ova.git /tmp/k8-ova
+cd /tmp/k8-ova/Release01/
+sudo cp  revproxy.tar.gz ~/
+cd /home/ubuntu/
+sudo tar xzvf revproxy.tar.gz
+cd reverse-proxy-icap-docker
+# stop docker containers
+sudo docker-compose down
+
+# start docker containers
+sudo docker-compose up -d
+
+# verify docker container is running
+sudo docker-compose ps
